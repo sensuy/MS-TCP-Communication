72a2e8eb5b5bdb56155cfdaba3c6b745bdd28c29
Sensui <sensuijunior@hotmail.com>
2023-04-14 23:07:36 -0300

    feat: first ms conncected

--- sample-communication/src/app.controller.ts
+++ sample-communication/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
+import { CreateUserEvent } from './create-user.event';
 
-@Controller()
+@Controller() 
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('user_created')
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('here');
+    
+   this.appService.handleUserCreated(data);
   }
 }
--- sample-communication/src/app.service.ts
+++ sample-communication/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('HandlerUserCreated - COMMUNICATIONS', data);
   }
 }
